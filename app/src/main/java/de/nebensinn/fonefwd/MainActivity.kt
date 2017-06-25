0e8985030ad2742b44134118f015ce0b1e240496
Martin Unzner <martin.u@posteo.de>
2017-06-25 18:41:22 +0200

    Initial interface

---
+++ app/src/main/java/de/nebensinn/fonefwd/MainActivity.kt
@@ -0,0 +1,12 @@
+package de.nebensinn.fonefwd
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
