82bdf607c2929c04bb8e665347cf493d56dc8e32
Martin Unzner <martin.u@posteo.de>
2017-08-05 20:19:04 +0200

    Add updateRule functionality to RuleModel

--- app/src/main/java/de/nebensinn/fonefwd/RuleModel.kt
+++ app/src/main/java/de/nebensinn/fonefwd/RuleModel.kt
@@ -1,5 +1,6 @@
 package de.nebensinn.fonefwd
 
+import android.renderscript.RSInvalidStateException
 import java.io.Serializable
 
 data class Rule(val wifiSSID: String, val targetPhoneNumber: String) : Serializable {
@@ -8,16 +9,28 @@ data class Rule(val wifiSSID: String, val targetPhoneNumber: String) : Serializa
 }
 
 interface IRuleModel {
-    fun addRule(rule: Rule?)
+    fun addRule(rule: Rule?): Int?
+    fun updateRule(indexString: String, changedRule: Rule)
 }
 
 class RuleModel() : IRuleModel {
-    private val ruleList = mutableSetOf<Rule>()
+    private val ruleList = mutableListOf<Rule>()
     val rules: List<Rule> get() = ruleList.toList()
 
-    override fun addRule(rule: Rule?) {
-        if (rule != null) {
-            ruleList.add(rule)
+    override fun addRule(rule: Rule?): Int? {
+        if (rule == null || ruleList.contains(rule)) {
+            return null
         }
+
+        ruleList.add(rule)
+        return ruleList.count() - 1
+    }
+
+    override fun updateRule(indexString: String, changedRule: Rule) {
+        val index = indexString.toInt()
+        if (index < 0 || index >= ruleList.count()) {
+            throw IllegalStateException()
+        }
+        ruleList[index] = changedRule
     }
 }
--- app/src/test/java/de/nebensinn/fonefwd/RuleModelUnitTest.kt
+++ app/src/test/java/de/nebensinn/fonefwd/RuleModelUnitTest.kt
@@ -1,8 +1,8 @@
 package de.nebensinn.fonefwd
 
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
 
 class RuleModelUnitTest {
     val ruleModel = RuleModel()
@@ -11,23 +11,58 @@ class RuleModelUnitTest {
     fun addRule_shouldAddNewRule() {
         assertEquals(emptyList<Rule>(), ruleModel.rules)
         val newRule = Rule("MyWifi", "12345")
-        ruleModel.addRule(newRule)
+        val index = ruleModel.addRule(newRule)
         assertTrue(ruleModel.rules.contains(newRule))
+        assertEquals(0, index)
         assertEquals(1, ruleModel.rules.count())
     }
 
     @Test
     fun addRule_shouldNotAddDuplicateRule() {
         val newRule = Rule("MyWifi", "12345")
-        ruleModel.addRule(newRule)
-        ruleModel.addRule(newRule)
+        assertEquals(0, ruleModel.addRule(newRule))
+        assertEquals(null, ruleModel.addRule(newRule))
         assertEquals(1, ruleModel.rules.count())
     }
 
     @Test
     fun addRule_shouldIgnoreNullRule() {
         assertEquals(emptyList<Rule>(), ruleModel.rules)
-        ruleModel.addRule(null)
+        assertEquals(null, ruleModel.addRule(null))
         assertEquals(emptyList<Rule>(), ruleModel.rules)
+    }
+
+    @Test(expected = NumberFormatException::class)
+    fun updateRule_shouldFailOnNonIntIndex() {
+        ruleModel.updateRule("twelve", Rule("", ""))
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun updateRule_shouldFailIfRuleDoesNotExist() {
+        ruleModel.addRule(Rule("MyWifi", "12345"))
+        ruleModel.updateRule("1", Rule("", ""))
+        ruleModel.updateRule("-22", Rule("", ""))
+        ruleModel.updateRule("22", Rule("", ""))
+    }
+
+    @Test
+    fun updateRule_shouldChangeExistingRule() {
+        val oldRule = Rule("MyWifi", "12345")
+        val ruleIndex = ruleModel.addRule(oldRule)
+        assertEquals(oldRule, ruleModel.rules[ruleIndex!!])
+
+        val newRule = Rule("MyNewWifi", "15764")
+        ruleModel.updateRule(ruleIndex.toString(), newRule)
+        assertEquals(newRule, ruleModel.rules[ruleIndex])
+    }
+
+    @Test
+    fun removeRule_shouldRemoveExistingRule() {
+
+    }
+
+    @Test
+    fun removeRule_shouldFailIfRuleDoesNotExist() {
+
     }
 }
